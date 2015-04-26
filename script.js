a4fa10f39a43431614ed024d3e94603521b43ebd
Rastanislav <asdf>
2015-04-26 02:49:06 +0200

    more changes

--- script.js
+++ script.js
@@ -9,70 +9,70 @@ $(document).ready(function(){
 	var shiftLeftLocationNeg = "400px";
 	var shiftTopLocationNeg = "-100px";
 
+
+
 	$('#bio').click(function(){
-		$(this).stop().animate({
-			"top": shiftTopLocationNeg
-		},function(){
-			$(this).css("visibility", "hidden")
-				   .css("left", defaultLeftLocation)
-				   .css("right", defaultLeftLocation)
-				   .css("top", defaultTopLocation);
-		});
-
-		$('#skills').css("top", shiftTopLocationPos)
-					.css("visibility", "visible")
-					.stop().animate({ "top": defaultTopLocation });
+		$(this).addClass('move-out-up');
+		//$('#skills').css("top", "-25%");
+		$('#skills').css("visibility", "visible");
+		$('#skills').addClass('move-in-down');
 	});
 
+	//$('#skills').click(function(){
+		//$(this).css("visibility", "visible");
+		//$(this).addClass('move-in-up');
+	//});
 
 
-	$('#skills').click(function(){
-		$(this).stop().animate({
-			"left": shiftLeftLocationNeg
-		},function(){
-			$(this).css("visibility", "hidden")
-				   .css("left", defaultLeftLocation)
-				   .css("right", defaultLeftLocation)
-				   .css("top", defaultTopLocation);
-		});
-
-		$('#projects').css("left", shiftLeftLocationPos)
-					  .css("visibility", "visible")
-					  .stop().animate({ "left": defaultLeftLocation });
-	});
 
 
-	$('#projects').click(function(){
-		$(this).stop().animate({ 
-			"top": shiftTopLocationPos
-		}, function(){
-				$(this).css("visibility", "hidden")
-					   .css("left", defaultLeftLocation)
-					   .css("right", defaultLeftLocation)
-					   .css("top", defaultTopLocation);
-		});
 
-		$('#contact').css("top", shiftTopLocationNeg)
-	   	   	   	   	 .css("visibility", "visible")
-	   	   	   	   	 .stop().animate({	"top": defaultTopLocation });
-	});
+	//$('#skills').click(function(){
+		//$(this).stop().animate({
+			//"left": shiftLeftLocationNeg
+		//},function(){
+			//$(this).css("visibility", "hidden")
+				   //.css("left", defaultLeftLocation)
+				   //.css("right", defaultLeftLocation)
+				   //.css("top", defaultTopLocation);
+		//});
+
+		//$('#projects').css("left", shiftLeftLocationPos)
+					  //.css("visibility", "visible")
+					  //.stop().animate({ "left": defaultLeftLocation });
+	//});
 
 
-	$('#contact').click(function(){
-		$(this).stop().animate({
-			"left": shiftLeftLocationPos
-		},function(){
-			$(this).css("visibility", "hidden")
-				   .css("left", defaultLeftLocation)
-				   .css("right", defaultLeftLocation)
-				   .css("top", defaultTopLocation);
-		});
+	//$('#projects').click(function(){
+		//$(this).stop().animate({ 
+			//"top": shiftTopLocationPos
+		//}, function(){
+				//$(this).css("visibility", "hidden")
+					   //.css("left", defaultLeftLocation)
+					   //.css("right", defaultLeftLocation)
+					   //.css("top", defaultTopLocation);
+		//});
 
-		$('#bio').css("left", shiftLeftLocationNeg)
-				 .css("visibility", "visible")
-				 .stop().animate({ "left": defaultLeftLocation });
+		//$('#contact').css("top", shiftTopLocationNeg)
+								 //.css("visibility", "visible")
+								 //.stop().animate({	"top": defaultTopLocation });
+	//});
 
-	});
+
+	//$('#contact').click(function(){
+		//$(this).stop().animate({
+			//"left": shiftLeftLocationPos
+		//},function(){
+			//$(this).css("visibility", "hidden")
+				   //.css("left", defaultLeftLocation)
+				   //.css("right", defaultLeftLocation)
+				   //.css("top", defaultTopLocation);
+		//});
+
+		//$('#bio').css("left", shiftLeftLocationNeg)
+				 //.css("visibility", "visible")
+				 //.stop().animate({ "left": defaultLeftLocation });
+	//});
 });
 
 
