523c0cb21f93c38e24b4d6fa3a93db362961bd44
Rastanislav <asdf>
2015-05-15 00:26:58 +0200

    started event w mousewheel. other shit works

--- script.js
+++ script.js
@@ -1,74 +1,132 @@
 $(document).ready(function(){
 
-var moveUp = function(id1, id2) {
-	$('#' + id1).addClass('move-out-up');
-	$('#' + id2).removeClass('move-out-left');
-	$('#' + id2).css("top", "125%");
+
+var moveDiv = function(id1, id2, direction) {
+	var options = {};
+	var edge;
+	var percent;
+
+	switch(direction){
+		case "up":
+			options = JSON.parse('{"top": "50%"}');
+			edge = "top";
+			percent = "150%";
+			break;
+		case "left":
+			options = JSON.parse('{"left": "50%"}');
+			edge = "left";
+			percent = "125%";
+			break;
+		case "down":
+			options = JSON.parse('{"top": "50%"}');
+			edge = "top";
+			percent = "-25%";
+			break;
+		case "right":
+			options = JSON.parse('{"left": "50%"}');
+			edge = "left";
+			percent = "-25%";
+			break;
+		default:
+			console.log("Wrong direction, dumbass...");
+	}
+
+	$('#' + id1).addClass('move-out-' + direction);
+	$('#' + id2).removeClass();
+	$('#' + id2).css(edge, percent);
 	$('#' + id2).css("visibility", "visible");
 	$('#' + id1).css("visibility", "hidden");
-	$('#' + id2).stop().animate({
-		"top": "50%"
-	}, 1000, function(){
+	$('#' + id2).stop().animate(
+		options, 1000, function(){
 	});
 };
 
+var moveUp =function(){
+	moveDiv('bio', 'skills', 'up');	
+} 
 
-var moveLeft = function(id1, id2) {
-	$('#' + id1).addClass('move-out-left');
-	$('#' + id2).removeClass('move-out-down');
-	$('#' + id2).css("left", "125%");
-	$('#' + id2).css("visibility", "visible");
-	$('#' + id1).css("visibility", "hidden");
-	$('#' + id2).stop().animate({
-		"left": "50%"
-	}, 1000, function(){
-	});
-};
+var moveLeft = function(){
+	moveDiv('skills', 'projects', "left");
+}	
 
+var moveDown = function(){
+	moveDiv('projects', 'contact', 'down');	
+}	
 
-var moveDown = function(id1, id2) {
-	$('#' + id1).addClass('move-out-down');
-	$('#' + id2).removeClass('move-out-right');
-	$('#' + id2).css("top", "-25%");
-	$('#' + id2).css("visibility", "visible");
-	$('#' + id1).css("visibility", "hidden");
-	$('#' + id2).stop().animate({
-		"top": "50%"
-	}, 1000, function(){
-	});
-};
+var moveRight = function(){
+	moveDiv('contact', 'bio', 'right');
+}	
 
 
-var moveRight = function(id1, id2) {
-	$('#' + id1).addClass('move-out-right');
-	$('#' + id2).removeClass('move-out-up');
-	$('#' + id2).css("left", "-25%");
-	$('#' + id2).css("visibility", "visible");
-	$('#' + id1).css("visibility", "hidden");
-	$('#' + id2).stop().animate({
-		"left": "50%"
-	}, 1000, function(){
-	});
-};
+
+//$('.active-div').on('DOMMouseScroll mousewheel', function (e) {
+  //if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
+	  ////scroll down
+		  //console.log('Down');
+
+		  //// get div from active-div class
+		  //// then using switch add necessary movements for divs
+		  //// in movement functions correct adding/removing active-div class. + use .next()
+		  ////var currentDiv = $('.active-div');
+		  ////var nextDiv = currentDiv.next();
+
+			////if(nextDiv.length == 0) {
+				////nextDiv = $('.slide').first();
+			  ////}
+
+		  ////switch($('.active-div').attr('id')){
+			  ////case 'bio':
+				  ////break;
+			  ////case 'skills':
+				  ////break;
+			  ////case 'projects':
+				  ////break;
+			  ////case 'contact':
+				  ////break;
+
+		  //}
+  //} else {
+	  ////scroll up
+	  //console.log('Up');
+  //}
+  ////prevent page fom scrolling
+  //return false;
+//});
+
+
+
 
 
 	$('#bio').click(function(){
-		moveUp('bio', 'skills');	
+		//moveUp();	
+		moveDiv('bio', 'skills', 'up');	
+		//$(this).off().removeClass();
+		//setTimeout(function(){ 
+		//$('#bio').click(moveUp) }, 3000); 
 	});
-	
+				
 
 	$('#skills').click(function(){
-		moveLeft('skills', 'projects');	
+		moveLeft();	
+		//$(this).off();
+		//setTimeout(function(){ 
+		//$('#skills').click(moveLeft) }, 3000); 
 	});
 
 
 	$('#projects').click(function(){
-		moveDown('projects', 'contact');	
+		moveDown();	
+		//$(this).off();
+		//setTimeout(function(){ 
+		//$('#projects').click(moveDown) }, 3000); 
 	});
 
 
 	$('#contact').click(function(){
-		moveRight('contact', 'bio');
+		moveRight();	
+		//$(this).off();
+		//setTimeout(function(){ 
+		//$('#contact').click(moveRight) }, 3000); 
 	});
 
 });
